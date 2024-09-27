ad1c2daee8d308a1c4af179b50894d86b750b702
Theo2801 <theologos.bourloglou@gmail.com>
2024-09-27 04:05:48 +0300

    [feat] Initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const data = () => {
+    return {
+        /* General */
+        bgColor: "#ffffff",
+        headerDesktopBgColor: "#ffffff",
+        headerDesktopItemsColor: this.mobileMenuButton,
+        headerMobileItemsColor: "#ffffff",
+        mobileMenuBgColor: "#171717",
+        mobileMenuDivideColor: this.headerMobileItemsColor,
+        mobileMenuButton: "#171717",
+        mobileMenuX: "#ffffff",
+        copyrightText: "#171717",
+
+        /* Category Buttons */
+        categoryBtnsBgColor: "#ffffff",
+        categoryBtnBgColor: "#ffffff",
+        categoryBtnColor: "#171717",
+        activeCategoryBtnBgColor: "#171717",
+        activeCategoryBtnColor: "#ffffff",
+        categoryBtnBorderColor: "transparent",
+        categoryBtnBorderWidth: "0px",
+
+        /* Categories */
+        categoryBgColor: "transparent",
+        categoryBorder: "none",
+        categoryBorderRadius: "0px",
+        categoryTitleBgColor: "transparent",
+        categoryTitleColor: "#171717",
+        categoryTitleSize: "36px",
+        categoryTitleAlign: "left",
+        categoryTitleTransform: "none",
+        categoryTitleBorderBottom: "none",
+        categoryDescriptionColor: "#171717",
+        categoryDescriptionAlign: "left",
+        categoryDescriptionTransform: "none",
+        categoryDescriptionStyle: "none",
+        categoryBodyPadding: "0px",
+
+        /* Products */
+        productBgColor: "transparent",
+        productBorderRadius: "0px",
+        productImageBorderRadius: "8px",
+        productTitleColor: "#171717",
+        productDescriptionColor: "#171717",
+        productPriceColor: "#171717",
+
+        loadData() {
+
+        },
+    };
+};
