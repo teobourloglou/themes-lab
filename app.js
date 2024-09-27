6f708b3636a44a36e4fa2b8c76a2a245bd39280d
Theo2801 <theologos.bourloglou@gmail.com>
2024-09-27 18:27:07 +0300

    [feat] Add generation of CSS file

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const data = () => {
     return {
+        themeName: "new-theme",
+
         /* General */
         bgColor: "#ffffff",
         headerDesktopBgColor: "#ffffff",
@@ -54,8 +56,61 @@ const data = () => {
         productDescriptionColor: "#171717",
         productPriceColor: "#171717",
 
-        loadData() {
+        cssString() {
 
+        return `
+.${this.themeName} {
+
+    /* General */
+    --bg-color: ${this.bgColor};
+    --header-desktop-bg-color: ${this.headerDesktopBgColor};
+    --header-desktop-items-color: ${this.headerDesktopItemsColor};
+    --header-mobile-items-color: ${this.headerMobileItemsColor};
+    --mobile-menu-bg-color: ${this.mobileMenuBgColor};
+    --mobile-menu-divide-color: ${this.mobileMenuDivideColor};
+    --mobile-menu-button: ${this.mobileMenuButton};
+    --mobile-menu-x: ${this.mobileMenuX};
+    --copyright-text: ${this.copyrightText};
+
+    /* Category Buttons */
+    --category-btns-bg-color: ${this.categoryBtnsBgColor};
+    --category-btn-bg-color: ${this.categoryBtnBgColor};
+    --category-btn-color: ${this.categoryBtnColor};
+    --active-category-btn-bg-color: ${this.activeCategoryBtnBgColor};
+    --active-category-btn-color: ${this.activeCategoryBtnColor};
+    --category-btn-border-color: ${this.categoryBtnBorderColor};
+    --category-btn-border-width: ${this.categoryBtnBorderWidth};
+
+    /* Categories */
+    --category-bg-color: ${this.categoryBgColor};
+    --category-border: ${this.categoryBorder};
+    --category-border-radius: ${this.categoryBorderRadius};
+    --category-title-bg-color: ${this.categoryTitleBgColor};
+    --category-title-color: ${this.categoryTitleColor};
+    --category-title-size: ${this.categoryTitleSize};
+    --category-title-align: ${this.categoryTitleAlign};
+    --category-title-transform: ${this.categoryTitleTransform};
+    --category-title-border-bottom: ${this.categoryTitleBorderBottom};
+    --category-description-color: ${this.categoryDescriptionColor};
+    --category-description-align: ${this.categoryDescriptionAlign};
+    --category-description-transform: ${this.categoryDescriptionTransform};
+    --category-description-style: ${this.categoryDescriptionStyle};
+    --category-body-padding: ${this.categoryBodyPadding};
+
+    /* Products */
+    --product-bg-color: ${this.productBgColor};
+    --product-border-radius: ${this.productBorderRadius};
+    --product-image-border-radius: ${this.productImageBorderRadius};
+    --product-title-color: ${this.productTitleColor};
+    --product-description-color: ${this.productDescriptionColor};
+    --product-price-color: ${this.productPriceColor};
+}`
         },
+
+        openCssInNewTab() {
+            window.open().document.write(`<pre>${this.cssString()}</pre>`);
+            newTab.document.close();  // Close the document stream to complete writing
+        }
+
     };
 };
